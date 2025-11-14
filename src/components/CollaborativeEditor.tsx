85988749a20a15095824d5024f1c5904c28beb5a
ratneshO5 <ratnesh.soni012@gmail.com>
2025-11-14 16:06:13 +0530

    Initial commit from create-liveblocks-app

---
+++ src/components/CollaborativeEditor.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { getYjsProviderForRoom } from "@liveblocks/yjs";
+import { useRoom } from "@liveblocks/react/suspense";
+import { useCallback, useEffect, useState } from "react";
+import styles from "./CollaborativeEditor.module.css";
+import { Avatars } from "@/components/Avatars";
+import { Editor } from "@monaco-editor/react";
+import { editor } from "monaco-editor";
+import { MonacoBinding } from "y-monaco";
+import { Awareness } from "y-protocols/awareness";
+import { Cursors } from "@/components/Cursors";
+import { Toolbar } from "@/components/Toolbar";
+
+// Collaborative code editor with undo/redo, live cursors, and live avatars
+export function CollaborativeEditor() {
+  const room = useRoom();
+  const provider = getYjsProviderForRoom(room);
+  const [editorRef, setEditorRef] = useState<editor.IStandaloneCodeEditor>();
+
+  // Set up Liveblocks Yjs provider and attach Monaco editor
+  useEffect(() => {
+    let binding: MonacoBinding;
+
+    if (editorRef) {
+      const yDoc = provider.getYDoc();
+      const yText = yDoc.getText("monaco");
+
+      // Attach Yjs to Monaco
+      binding = new MonacoBinding(
+        yText,
+        editorRef.getModel() as editor.ITextModel,
+        new Set([editorRef]),
+        provider.awareness as unknown as Awareness
+      );
+    }
+
+    return () => {
+      binding?.destroy();
+    };
+  }, [editorRef, room]);
+
+  const handleOnMount = useCallback((e: editor.IStandaloneCodeEditor) => {
+    setEditorRef(e);
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {provider ? <Cursors yProvider={provider} /> : null}
+      <div className={styles.editorHeader}>
+        <div>{editorRef ? <Toolbar editor={editorRef} /> : null}</div>
+        <Avatars />
+      </div>
+      <div className={styles.editorContainer}>
+        <Editor
+          onMount={handleOnMount}
+          height="100%"
+          width="100hw"
+          theme="vs-light"
+          defaultLanguage="typescript"
+          defaultValue=""
+          options={{
+            tabSize: 2,
+            padding: { top: 20 },
+          }}
+        />
+      </div>
+    </div>
+  );
+}
